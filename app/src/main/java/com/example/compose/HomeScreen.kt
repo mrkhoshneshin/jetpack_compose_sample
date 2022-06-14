7d257229fbb3a7da4d76b8a88fcdf82fcff20861
mrkhoshneshin <mrkhoshneshin1@gmail.com>
2022-06-14 05:42:46 -0400

    a

--- app/src/main/java/com/example/compose/HomeScreen.kt
+++ app/src/main/java/com/example/compose/HomeScreen.kt
@@ -1,6 +1,9 @@
 package com.example.compose
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -8,6 +11,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -17,101 +22,47 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose.data.Chat
 import com.example.compose.ui.theme.ComposeTheme
 
 @Composable
 fun HomeScreen() {
-    ChatSection(
-        listOf(
-            Chat(
-                R.drawable.icon,
-                "this is a text wrote by mohammadjavad khoshneshin and it's my first course of android jetpack compose and you can contact me with sending an email to mohammadjavadKhoshneshin@gmail.com"
-            ),
-            Chat(
-                R.drawable.icon,
-                "this is a text wrote by mohammadjavad khoshneshin and it's my first course of android jetpack compose and you can contact me with sending an email to mohammadjavadKhoshneshin@gmail.com"
-            ),
-            Chat(
-                R.drawable.icon,
-                "this is a text wrote by mohammadjavad khoshneshin and it's my first course of android jetpack compose and you can contact me with sending an email to mohammadjavadKhoshneshin@gmail.com"
-            ),
-            Chat(
-                R.drawable.icon,
-                "this is a text wrote by mohammadjavad khoshneshin and it's my first course of android jetpack compose and you can contact me with sending an email to mohammadjavadKhoshneshin@gmail.com"
-            ),
-            Chat(
-                R.drawable.icon,
-                "this is a text wrote by mohammadjavad khoshneshin and it's my first course of android jetpack compose and you can contact me with sending an email to mohammadjavadKhoshneshin@gmail.com"
-            ),
-            Chat(
-                R.drawable.icon,
-                "this is a text wrote by mohammadjavad khoshneshin and it's my first course of android jetpack compose and you can contact me with sending an email to mohammadjavadKhoshneshin@gmail.com"
-            ),
-            Chat(
-                R.drawable.icon,
-                "this is a text wrote by mohammadjavad khoshneshin and it's my first course of android jetpack compose and you can contact me with sending an email to mohammadjavadKhoshneshin@gmail.com"
-            ), Chat(
-                R.drawable.icon,
-                "this is a text wrote by mohammadjavad khoshneshin and it's my first course of android jetpack compose and you can contact me with sending an email to mohammadjavadKhoshneshin@gmail.com"
-            ), Chat(
-                R.drawable.icon,
-                "this is a text wrote by mohammadjavad khoshneshin and it's my first course of android jetpack compose and you can contact me with sending an email to mohammadjavadKhoshneshin@gmail.com"
-            ),
-            Chat(
-                R.drawable.icon,
-                "this is a text wrote by mohammadjavad khoshneshin and it's my first course of android jetpack compose and you can contact me with sending an email to mohammadjavadKhoshneshin@gmail.com"
-            )
-
-
-        )
-    )
-}
-
-@Composable
-fun ChatSection(chats: List<Chat>) {
-    LazyColumn(modifier = Modifier.padding(15.dp)) {
-        items(chats.size) {
-            ChatItem(chat = chats[it])
+    LazyColumn {
+        items(100) {
+            Greeting("MohammadJavad")
         }
     }
 }
 
+@Preview(showBackground = true, widthDp = 320)
 @Composable
-fun ChatItem(chat: Chat) {
-    var selected by remember {
+fun Greeting(name: String = "MohammadJavad") {
+    var expanded by remember {
         mutableStateOf(false)
     }
-    Row(
-        horizontalArrangement = Arrangement.Start,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(bottom = 10.dp)
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.icon),
-            contentDescription = chat.text,
-            modifier = Modifier
-                .clip(CircleShape)
-                .size(32.dp)
-                .border(if(selected) 1.dp else 0.dp, Color.Green, CircleShape)
+    val expandDp by animateDpAsState(
+        targetValue = if (expanded) 45.dp else 1.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
         )
-        Spacer(modifier = Modifier.size(8.dp))
-        Column(
-            modifier = Modifier.clickable { selected = !selected }
+    )
+    Surface(color = MaterialTheme.colors.primary) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .padding(start = 10.dp, end = 10.dp, top = 10.dp, bottom = expandDp.coerceAtLeast(0.dp))
+                .fillMaxWidth()
         ) {
-            Text(
-                text = "Halino",
-                color = if (selected) Color.Blue else Color.Black,
-                fontWeight = FontWeight.Bold
-            )
-            AnimatedVisibility(visible = selected) {
-                Text(
-                    text = chat.text,
-                    color = if (selected) Color.Blue else Color.Black,
-                    style = MaterialTheme.typography.body1,
-                )
+            Column {
+                Text(text = "Welcome")
+                Text(text = name)
+            }
+            OutlinedButton(onClick = { expanded = !expanded }) {
+                Text(text = if(expanded) "Show less" else "Show more")
             }
         }
     }
