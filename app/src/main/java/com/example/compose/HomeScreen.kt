4074eaccc9a08c8a0e40b6c75276a109ee41d5e8
mrkhoshneshin <mrkhoshneshin1@gmail.com>
2022-06-17 14:46:12 -0400

    implement_search_bar

--- app/src/main/java/com/example/compose/HomeScreen.kt
+++ app/src/main/java/com/example/compose/HomeScreen.kt
@@ -1,52 +1,51 @@
 package com.example.compose
 
-import androidx.compose.animation.core.AnimationSpec
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.compose.ui.theme.ComposeTheme
 
 @Composable
 fun HomeScreen() {
-    Surface() {
-        LazyColumn {
-            items(20) {
-                ContentItem()
-            }
+    ComposeTheme {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            SearchBar(Modifier)
+            SportCourcesSection()
+            FavoriteCollection()
         }
     }
 }
 
 @Composable
-fun ContentItem() {
-    var expanded by remember {
-        mutableStateOf(false)
-    }
-    val expandDp by animateDpAsState(targetValue = if (expanded) 45.dp else 0.dp, animationSpec = spring(
-        Spring.DampingRatioMediumBouncy,Spring.StiffnessLow
-    ))
+fun FavoriteCollection() {
 
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween,
-        modifier = Modifier.fillMaxWidth().padding(start = 24.dp, end = 24.dp, top = 24.dp, bottom = expandDp.coerceAtLeast(0.dp)).background(Color.Green)
-    ) {
-        Column(modifier = Modifier.width(100.dp)) {
-            Text(text = "Hi MohammadJavad")
-            Text(text = "Hi This is a goolds;lfksd dsfdsf;ldksj sdf sd;flkdjsf;lsdkjf ;sldkfjsd;lfkjsd;flksjdflksdjfsdflksdjhfsdlkjfhsdfkj")
-        }
-        OutlinedButton(onClick = {expanded = !expanded}) {
-            Text(text = if (expanded) "Show less" else "Show more")
-        }
-        }
-    }
+}
+
+@Composable
+fun SearchBar(modifier: Modifier) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {Icon(imageVector = Icons.Default.Search, contentDescription = null)},
+        colors = TextFieldDefaults.textFieldColors(backgroundColor = MaterialTheme.colors.surface),
+        placeholder = { Text(stringResource(id = R.string.place_holder_search))},
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .padding(10.dp)
+            .heightIn(min = 56.dp)
+    )
+}
+
+@Composable
+fun SportCourcesSection() {
+
+}
